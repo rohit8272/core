0406e90cdb4704eb8cb9f85fc19a3dacdeddd633
Rohit Sharma <158131370+rohit8272@users.noreply.github.com>
2024-03-22 16:51:12 +0530

    Update settings.py

--- core/settings.py
+++ core/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-f9ax&8&4sffx1b3-du&gcsydcb6lk!%t+jfs@bhqj!#a7z*9jg
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app' , '.now.sh' , '127.0.0.1' , 'localhost']
+ALLOWED_HOSTS = ['.vercel.app' , '.now.sh']
 
 
 # Application definition
@@ -120,8 +120,11 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
+import os
 
 STATIC_URL = 'static/'
+STATICSFILES_DIRS = os.path.join(BASE_DIR , 'static')
+STATIC_ROOT = os.path.join(BASE_DIR , 'staticfiles' ,'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field

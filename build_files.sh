8c01419cb1f53b3b7dbd6989da4b324b6d727082
Rohit Sharma <158131370+rohit8272@users.noreply.github.com>
2024-03-22 18:09:22 +0530

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -1,2 +1,19 @@
+apt-get update \
+    && apt-get upgrade -y \
+    && apt-get install -y \
+        build-essential \
+        libssl-dev \
+        libffi-dev \
+        python3-dev \
+        build-essential \
+        libjpeg-dev \
+        zlib1g-dev \
+        gcc \
+        libpq-dev \
+        libc-dev \
+        bash \
+        git \
+    && pip3 install --upgrade pip
+
 pip install -r requirements.txt
 python3.9 manage.py collectstatic --noinput
